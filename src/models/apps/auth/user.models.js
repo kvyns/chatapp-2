31660a54414d075f9850a895d1f8734070093b58
Kavyansh <kd.kavyansh2003@gmail.com>
2024-09-20 22:26:18 +0530

    final changes

--- src/models/apps/auth/user.models.js
+++ src/models/apps/auth/user.models.js
@@ -83,33 +83,8 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.post("save", async function (user, next) {
-  // ! Generally, querying data on every user save is not a good idea and not necessary when you are working on a specific application which has concrete models which are tightly coupled
-  // ! However, in this application this user model is being referenced in many loosely coupled models so we need to do some initial setups before proceeding to make sure the data consistency and integrity
-  const ecomProfile = await EcomProfile.findOne({ owner: user._id });
-  const socialProfile = await SocialProfile.findOne({ owner: user._id });
-  const cart = await Cart.findOne({ owner: user._id });
 
-  // Setup necessary ecommerce models for the user
-  if (!ecomProfile) {
-    await EcomProfile.create({
-      owner: user._id,
-    });
-  }
-  if (!cart) {
-    await Cart.create({
-      owner: user._id,
-      items: [],
-    });
-  }
-
-  // Setup necessary social media models for the user
-  if (!socialProfile) {
-    await SocialProfile.create({
-      owner: user._id,
-    });
-  }
-  next();
-});
+  });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
